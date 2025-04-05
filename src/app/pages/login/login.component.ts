ab8a2e0ce68fec2ac0a543adb0970489abd1a75c
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-04-05 11:53:02 -0500

    actualizando login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -8,27 +7,16 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
-  errorMessage: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private router: Router) {}
 
   onLogin() {
-    this.authService.login(this.username, this.password).subscribe(
-      (response) => {
-        // Si la autenticación es exitosa, redirige a la página de inicio
-        this.router.navigate(['/inicio']);
-      },
-      (error) => {
-        // Muestra un mensaje de error si el login falla
-        this.errorMessage = 'Usuario o contraseña incorrectos';
-        console.error('Error de autenticación:', error);
-      }
-    );
+    this.router.navigate(['/inicio']);
   }
 }
